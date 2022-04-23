aabf80e530ae54b8fa32c70cad3645ccadfe1b27
4b <4b@INF10>
2022-04-23 12:31:44 +0200

    versione 1.00

---
+++ src/Principale.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author rocco mele
+ * 
+ * @version 1.00
+ *modificato online
+ *
+ */
+public class Principale {
+
+	public static void main(String[] args) {
+		
+		System.out.println("rocco mele");
+
+	}
+
+}
